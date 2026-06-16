2f29244ff2fab580893a8daef2663146e2603c04
rosalesreyespaola73-arch <rosalesreyespaola73@gmail.com>
2026-06-15 23:56:44 -0500

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ dotenv.config({ path: path.join(__dirname, '..', '.env') });
 console.log('🔍 Configuración cargada:');
 
 export const DB_HOST = process.env.MYSQL_HOST || 'localhost';
-export const DB_DATABASE = process.env.MYSQL_DATABASE || 'base2026';
+export const DB_DATABASE = process.env.MYSQL_DATABASE || 'railway';  // ← CAMBIAR de 'base2026' a 'railway'
 export const DB_USER = process.env.MYSQL_USER || 'root';
 export const DB_PASSWORD = process.env.MYSQL_PASSWORD || '';
 export const DB_PORT = process.env.MYSQL_PORT || 3306;
